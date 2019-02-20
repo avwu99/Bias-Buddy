bbcf5b2466c0f0904fa36082e5e3c7ba9e9b1d5b
Christopher Walker <cyorir@gmail.com>
2019-02-19 21:25:38 -0600

    updated button behavior

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,31 @@
 bi = d3.select("#biasIndicator")
-moveleft = function() {
-    bi.attr("points","350,75 360,90 340,90")
+bm = d3.select("#biasMeter")
+bias_score = 0
+moveLeft = function() {
+    width = bm.attr("width")
+    bias_score = Math.max(-100, bias_score-20)
+    bix1 = String((bias_score+100)/200*width*0.9 + width*0.05)
+    bix2 = String((bias_score+100)/200*width*0.9 + width*0.05 + 10) 
+    bix3 = String((bias_score+100)/200*width*0.9 + width*0.05 - 10)
+    bi.attr("points",bix1+",75 "+ bix2+",90 "+bix3+",90")
 }
-movecenter = function() {
-    bi.attr("points","450,75 460,90 440,90")
+moveCenter = function() {
+    width = bm.attr("width")
+    if (bias_score > 0) {
+        bias_score = Math.max(0, bias_score-20)
+    } else {
+        bias_score = Math.min(0, bias_score+20)
+    }
+    bix1 = String((bias_score+100)/200*width*0.9 + width*0.05)
+    bix2 = String((bias_score+100)/200*width*0.9 + width*0.05 + 10) 
+    bix3 = String((bias_score+100)/200*width*0.9 + width*0.05 - 10)
+    bi.attr("points",bix1+",75 "+ bix2+",90 "+bix3+",90")
 }
-moveright = function() {
-    bi.attr("points","550,75 560,90 540,90")
+moveRight = function() {
+    width = bm.attr("width")
+    bias_score = Math.min(100, bias_score+20)
+    bix1 = String((bias_score+100)/200*width*0.9 + width*0.05)
+    bix2 = String((bias_score+100)/200*width*0.9 + width*0.05 + 10) 
+    bix3 = String((bias_score+100)/200*width*0.9 + width*0.05 - 10)
+    bi.attr("points",bix1+",75 "+ bix2+",90 "+bix3+",90")
 }
